23aeafe8bbe821673cee508b79aac45c3b9a2fe9
CarlosEduardoBorges <eduardoborges705@hotmail.com>
2026-03-19 21:20:12 -0300

    Aplicação do Vue

---
+++ Carlos Eduardo Rodrigues/Aulas/view_cli/src/components/HomeComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <footer class="footer">
+            <p>BEM VINDO: {{ nome }} OLÁ MUNDO DO <b>VUE COM CLI</b></p>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeComponent",
+        data() {
+            return {
+                nome: "CARLOS EDUARDO RODRIGUES BORGES GONÇALVES",
+            };
+        },
+    };
+</script>
