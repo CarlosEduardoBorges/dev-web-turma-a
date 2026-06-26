483d68aca2a37dd90625ca5443b17d297788866b
CarlosEduardoBorges <eduardoborges705@hotmail.com>
2026-06-26 16:45:09 -0300

    style: reidentifica sistema com logo, banner e paleta T-Sushi

---
+++ Carlos Eduardo Rodrigues Borges/t-sushi/src/components/BannerComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="main-banner">
+    <img src="~@/../public/img/BANNER_SUSHI.png" id="banner-img" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BannerComponent",
+};
+</script>
+
+<style scoped>
+#main-banner {
+  width: 100%;
+  background-color: #1a2744;
+  display: flex;
+  justify-content: center;
+}
+
+#banner-img {
+  width: 100%;
+  max-width: 1400px;
+  height: auto;
+  display: block;
+}
+</style>
